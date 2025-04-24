d09b5b593b07a75ce660354744851843944f552a
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-04-24 13:12:38 +0500

    schema

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,4 @@
-import mongoose , {Schema , Document} from "mongoose";
+ import mongoose , {Schema , Document} from "mongoose";
 
 export interface Message extends Document{
     content: string;
@@ -6,13 +6,75 @@ export interface Message extends Document{
 
 }
 
-const messageSchema: Schema = new Schema<Message>({ 
+const messageSchema: Schema<Message> = new Schema({ 
     content: {
         type: String,
         required: true,
     },
     createdAt: {
         type: Date,
+        required: true,
         default: Date.now,
     },
+
 })
+
+export interface User extends Document{
+     username: string;
+     email: string;
+     password: string;
+     verifyCode: string;
+     verifyCodeExpiry: Date;
+     isVerified: boolean;
+     isAccecptedMessage: boolean;
+     messages: Message[];
+
+
+}
+
+const UserSchema: Schema<User> = new Schema({ 
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        unique: true,
+
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address'],
+
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        // minlength: [6, 'Password must be at least 6 characters long'],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, 'verifyCode is required'],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, 'verifyCodeExpiry is required'],
+
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAccecptedMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [messageSchema],
+
+    
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User', UserSchema);
+
+export default UserModel;
---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+  import { z } from "zod"; 
+
+
+export const  messageSchema =  z.object({
+    content: z.string()
+    .min(10, { message: "content must be at least of 10 charaters" 
+    })
+            .max(300, { message: "content must be at most of 1000 charaters" })
+
+  })    
